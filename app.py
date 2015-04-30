6e1946e264bb2a2f3163a7d22c148de94b6f3c4f
Lucy Park <me@lucypark.kr>
2015-04-30 13:10:10 +0900

    Modify app.py to use with uwsgi

--- app.py
+++ app.py
@@ -39,50 +39,46 @@ def get_tags(text, minsyl=1, ntags=10, tagger='', posnv='N', stopwords=[]):
     return [{ 'text': n, 'size': c*multiplier } for n, c in count[:ntags]]
 
 
-def create_app():
-    app = Flask(__name__)
-    app.debug = SERVER_SETTINGS['debug']
+app = Flask(__name__)
+app.debug = SERVER_SETTINGS['debug']
 
-    @app.route('/', methods=['GET', 'POST'])
-    def home():
-        if request.method=='GET':
-            text = kolaw.open('constitution.txt').read()
-            stopwords = u'그,또는,한다'
-            init = True
-        else:
-            text = request.form.get('wordtext', type=unicode)
-            stopwords = request.form.get('stopwords', type=unicode)
-            init = False
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method=='GET':
+        text = kolaw.open('constitution.txt').read()
+        stopwords = u'그,또는,한다'
+        init = True
+    else:
+        text = request.form.get('wordtext', type=unicode)
+        stopwords = request.form.get('stopwords', type=unicode)
+        init = False
 
-        minsyl = request.form.get('minsyl', 1, type=int)
-        ntags = request.form.get('ntags', 10, type=int)
-        tagger = request.form.get('tagger', '', type=unicode)
-        posnv = request.form.get('noun', '', type=unicode)\
-              + request.form.get('verb', '', type=unicode)
+    minsyl = request.form.get('minsyl', 1, type=int)
+    ntags = request.form.get('ntags', 10, type=int)
+    tagger = request.form.get('tagger', '', type=unicode)
+    posnv = request.form.get('noun', '', type=unicode)\
+          + request.form.get('verb', '', type=unicode)
 
-        s = time.clock()
-        tags = get_tags(text, minsyl, ntags, tagger, posnv, stopwords.split(','))
-        return render_template('home.html',\
-                    init=init,
-                    text=text,
-                    tags=tags,
-                    time=time.clock()-s,
-                    textlen=len(text),
-                    stopwords=stopwords,
-                    ntags=len(tags))
+    s = time.clock()
+    tags = get_tags(text, minsyl, ntags, tagger, posnv, stopwords.split(','))
+    return render_template('home.html',\
+                init=init,
+                text=text,
+                tags=tags,
+                time=time.clock()-s,
+                textlen=len(text),
+                stopwords=stopwords,
+                ntags=len(tags))
 
-    @app.route('/faq')
-    def faq():
-        return render_template('faq.html')
+@app.route('/faq')
+def faq():
+    return render_template('faq.html')
 
-    @app.context_processor
-    def inject_vars():
-        return dict(BRAND=BRAND)
-
-    return app
+@app.context_processor
+def inject_vars():
+    return dict(BRAND=BRAND)
 
 
 if __name__ == '__main__':
-    app = create_app()
     port = int(os.environ.get('PORT', SERVER_SETTINGS['port']))
     app.run(SERVER_SETTINGS['host'], port)
